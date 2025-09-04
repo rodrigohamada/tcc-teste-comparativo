87379bc0bc5b67a5766c2fa7bf09ea498d11bd87
Rodrigo Hamada <104478788+rodrigohamada@users.noreply.github.com>
2025-09-03 21:10:31 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# Módulo de configuração com segredos expostos.
+
+# Detectado por: Gitleaks
+SENHA_BANCO_DE_DADOS = "admin123#@!"
+CHAVE_AWS_SECRETA = "AKIAIOSFODNN7EXAMPLE" # Padrão de chave da AWS
